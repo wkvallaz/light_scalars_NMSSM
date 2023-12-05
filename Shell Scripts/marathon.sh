c0add675d364a68e6872954f7e239013a8dac826
wkvallaz <93166360+wkvallaz@users.noreply.github.com>
2023-12-04 21:42:29 -0700

    up

--- Shell Scripts/marathon.sh
+++ Shell Scripts/marathon.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -e
 	#/usr/bin/python3
 	#1/usr/bin/expect
 	#1/usr/bin/python3 -u
 # shell script if need to run several nmssm inp files
 # REQUIRES make init AND make TO ALREADY BE RUN BEFORE
 #for run in "wH" "wh" "wd" "wR" "wr" "wK"
+if [ -f "/home/wolf/NMSSMTools_6.0.0/main/nmhdecay_rand.f" ]
+then	
+	echo Initiating runs...
+else
+	echo Initializing...
+	make init
+	make
+	echo Initiating runs...
+fi
+
 for run in "$@"
 do
 	for con in "THY" "LEP" "LHC" "BKF"
@@ -14,4 +24,4 @@ do
 	done
 done
 date
-echo finito
+echo -e Runs complete."\n- --- - --- - --- - --- -"
