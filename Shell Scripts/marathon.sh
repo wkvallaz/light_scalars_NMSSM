54b765c7bca04761da05ed0b285b3cd2cad8b839
wkvallaz <93166360+wkvallaz@users.noreply.github.com>
2023-12-06 13:51:17 -0700

    up

--- Shell Scripts/marathon.sh
+++ Shell Scripts/marathon.sh
@@ -5,16 +5,20 @@
 # shell script if need to run several nmssm inp files
 # REQUIRES make init AND make TO ALREADY BE RUN BEFORE
 #for run in "wH" "wh" "wd" "wR" "wr" "wK"
-if [ -f "/home/wolf/NMSSMTools_6.0.0/main/nmhdecay_rand.f" ]
-then	
-	echo Initiating runs...
-else
-	echo Initializing...
-	make init
+if [ -f "/home/wolf/NMSSMTools_6.0.0/main/nmhdecay_rand.o" ] #### after make init, sources/sources.a exists
+then							     #### after make, main/nmhdecay_rand.o exists
+	:
+elif [ -f "/home/wolf/NMSSMTools_6.0.0/sources/sources.a" ]
+then
+	echo Initializing code...
 	make
-	echo Initiating runs...
+else
+	echo Initializing sources...
+	make init
+	echo Initializing code...
+	make
 fi
-
+echo Initiating runs...
 for run in "$@"
 do
 	for con in "THY" "LEP" "LHC" "BKF"
